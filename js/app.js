ec98233c941f16667f5feead9486501f56bb8484
Simon Ong <simonongst@gmail.com>
2025-07-24 20:45:53 +0800

    create function for play, feed, sleep. addEventListner type click to reset state to zero and invoke render

--- js/app.js
+++ js/app.js
@@ -3,45 +3,85 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let boredom = 0;
-let hunger = 0;
-let sleepiness = 0;
+let state = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0,
+}
 let timer;
 let gameOver = false;
 
 /*------------------------ Cached Element References ------------------------*/
-const boredomStatEl = document.getElementById('boredom-stat');
-const hungerStatEl = document.getElementById('hunger-stat');
-const sleepinessStatEl = document.getElementById('sleepiness-stat');
-const playBtnEl = document.getElementById('play');
-const feedBtnEl = document.getElementById('feed');
-const sleepBtnEl = document.getElementById('sleep');
-const gameMessageEl = document.getElementById('message');
-const resetBtnEl = document.getElementById('restart');
+const boredomStatEl = document.querySelector('#boredom-stat');
+const hungerStatEl = document.querySelector('#hunger-stat');
+const sleepinessStatEl = document.querySelector('#sleepiness-stat');
+
+const playBtnEl = document.querySelector('#play');
+const feedBtnEl = document.querySelector('#feed');
+const sleepBtnEl = document.querySelector('#sleep');
+
+const gameMessageEl = document.querySelector('#message');
+const resetBtnEl = document.querySelector('#restart');
 
 /*-------------------------------- Functions --------------------------------*/
-function init(){
-    timer = setInterval(runGame(), 2000);
+const render = () => {
+    boredomStatEl.textContent = state.boredom;
+    hungerStatEl.textContent = state.hunger;
+    sleepinessStatEl.textContent = state.sleepiness;
+
+    if(gameOver === true){
+        clearInterval(timer);
+        resetBtnEl.classList.remove("hidden");
+        gameMessageEl.classList.remove("hidden");
+    };
+};
+
+const updateStates = () => {
+    state.boredom += Math.ceil(Math.random(state.boredom) * 3);
+    state.hunger += Math.ceil(Math.random(state.hunger) * 3);
+    state.sleepiness += Math.ceil(Math.random(state.sleepiness) * 3);
+};
+
+const checkGameOver = () => {
+    if(state.boredom >= 10){
+        gameOver = true;
+    };
+    if(state.hunger >= 10){
+        gameOver = true;
+    };
+    if(state.sleepiness >= 10){
+        gameOver = true;
+    };
+};
+
+const playBtnClick = () => {
+    state.boredom = 0;
+    render();
+};
+
+const feedBtnClick = () => {
+    state.hunger = 0;
+    render();
+};
+
+const sleepBtnClick = () => {
+    state.sleepiness = 0;
+    render();
+};
+
+const runGame = () => {
+    updateStates();
+    checkGameOver();
+    render();
+};
+
+const init = () => {
+    timer = setInterval(runGame, 2000);
 };
 
 init();
 
-function runGame(){
-    updateStates();
-    render();
-};
-
-
-function render(){
-
-};
-
-function updateStates(){
-    boredom = Math.ceil(Math.random(boredom) * 3);
-    hunger = Math.ceil(Math.random(hunger) * 3);
-    sleepiness = Math.ceil(Math.random(sleepiness) * 3);
-};
-
 /*----------------------------- Event Listeners -----------------------------*/
-
-
+playBtnEl.addEventListener('click', playBtnClick);
+feedBtnEl.addEventListener('click', feedBtnClick);
+sleepBtnEl.addEventListener('click', sleepBtnClick);
